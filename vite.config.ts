300073223fbe0d6716e9578f820b449c9bf1a37e
马文卿 <529837505@qq.com>
2022-10-31 14:00:58 +0800

    less config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,41 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { normalizePath } from 'vite';
+import { defineConfig } from 'vite';
+import path from 'path';
+import react from '@vitejs/plugin-react';
+
+const variablePath = normalizePath(path.resolve('./src/assets/variable.less'));
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 5000,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    },
+  },
+  css: {
+    postcss: {
+      plugins: [
+        {
+          postcssPlugin: "internal:charset-removal",
+          AtRule: {
+            charset: (atRule) => {
+              if (atRule.name === "charset") {
+                atRule.remove();
+              }
+            },
+          },
+        },
+      ],
+    },
+    preprocessorOptions: {
+      less: {
+        javascriptEnabled: true,
+        additionalData: `@import "${variablePath}";`
+      },
+    }
+  },
   plugins: [react()]
 })
