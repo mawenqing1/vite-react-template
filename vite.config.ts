fa399cfd4696c4e7027a657ef801ee59217babbb
马文卿 <529837505@qq.com>
2022-10-31 14:40:22 +0800

    eslint cinfig

--- vite.config.ts
+++ vite.config.ts
@@ -2,40 +2,44 @@ import { normalizePath } from 'vite';
 import { defineConfig } from 'vite';
 import path from 'path';
 import react from '@vitejs/plugin-react';
+import viteEslint from 'vite-plugin-eslint';
 
 const variablePath = normalizePath(path.resolve('./src/assets/variable.less'));
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    port: 5000,
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src')
-    },
-  },
-  css: {
-    postcss: {
-      plugins: [
-        {
-          postcssPlugin: "internal:charset-removal",
-          AtRule: {
-            charset: (atRule) => {
-              if (atRule.name === "charset") {
-                atRule.remove();
-              }
-            },
-          },
-        },
-      ],
-    },
-    preprocessorOptions: {
-      less: {
-        javascriptEnabled: true,
-        additionalData: `@import "${variablePath}";`
-      },
-    }
-  },
-  plugins: [react()]
-})
+	server: {
+		port: 5000,
+	},
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, 'src')
+		},
+	},
+	css: {
+		postcss: {
+			plugins: [
+				{
+					postcssPlugin: 'internal:charset-removal',
+					AtRule: {
+						charset: (atRule) => {
+							if (atRule.name === 'charset') {
+								atRule.remove();
+							}
+						},
+					},
+				},
+			],
+		},
+		preprocessorOptions: {
+			less: {
+				javascriptEnabled: true,
+				additionalData: `@import "${variablePath}";`
+			},
+		}
+	},
+	plugins: [
+		react(),
+		viteEslint(),
+	]
+});
