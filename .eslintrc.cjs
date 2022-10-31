0ee8d4ce744e9524468312512abb57de3f7898a4
马文卿 <529837505@qq.com>
2022-10-31 14:40:38 +0800

    eslint config

---
+++ .eslintrc.cjs
@@ -0,0 +1,36 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    settings: {
+        react: {
+          version: '999.999.999'
+        }
+      },
+    overrides: [],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true
+          },
+        ecmaVersion: "latest",
+        sourceType: "module",
+    },
+    plugins: ["react", "@typescript-eslint"],
+    rules: {
+        indent: ["error", "tab"],
+        "linebreak-style": ["error", "unix"],
+        quotes: ["error", "double"],
+        semi: ["error", "always"],
+        "react/react-in-jsx-scope": "off",
+        "no-cond-assign": ["error", "always"],
+        "@typescript-eslint/ban-ts-comment": "error",
+        "@typescript-eslint/no-explicit-any": "warn",
+    },
+};
